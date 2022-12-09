7ba8194b9cc3c5b741ed7f4552ed085d19eca3e1
mueseb <mueseb18@htl-kaindorf.at>
2022-12-09 15:20:30 +0100

    init commit

---
+++ src/main/java/net/fabricmc/carpettrapdoors/CarpetTrapdoorMain.java
@@ -0,0 +1,20 @@
+package net.fabricmc.carpettrapdoors;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.carpettrapdoors.registry.ModBlocks;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+
+public class CarpetTrapdoorMain implements ModInitializer {
+
+	public static final String MOD_ID = "carpettrapdoors";
+	public static final ItemGroup CARPET_TRAPDOORS = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "carpet_trapdoors"), () -> new ItemStack(Blocks.RED_CARPET));
+
+	@Override
+	public void onInitialize() {
+		ModBlocks.registerModBlocks();
+	}
+}
