267cd590fb85f9223e010ccf3abbb6cc6a4c0d54
mueseb <mueseb18@htl-kaindorf.at>
2022-12-11 00:09:48 +0100

    recipe fix

--- src/main/java/net/fabricmc/carpettrapdoors/registry/ModBlocks.java
+++ src/main/java/net/fabricmc/carpettrapdoors/registry/ModBlocks.java
@@ -11,43 +11,11 @@ import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
+import java.util.Arrays;
+
 public class ModBlocks {
 
-    public static final Block RED_CARPET_TRAPDOOR = registerBlock("red_carpet_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.CARPET).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block WHITE_CARPET_TRAPDOOR = registerBlock("white_carpet_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.CARPET).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block ORANGE_CARPET_TRAPDOOR = registerBlock("orange_carpet_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.CARPET).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block MAGENTA_CARPET_TRAPDOOR = registerBlock("magenta_carpet_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.CARPET).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block LIGHT_BLUE_CARPET_TRAPDOOR = registerBlock("light_blue_carpet_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.CARPET).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block YELLOW_CARPET_TRAPDOOR = registerBlock("yellow_carpet_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.CARPET).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block LIME_CARPET_TRAPDOOR = registerBlock("lime_carpet_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.CARPET).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block PINK_CARPET_TRAPDOOR = registerBlock("pink_carpet_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.CARPET).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block GRAY_CARPET_TRAPDOOR = registerBlock("gray_carpet_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.CARPET).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block LIGHT_GRAY_CARPET_TRAPDOOR = registerBlock("light_gray_carpet_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.CARPET).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block CYAN_CARPET_TRAPDOOR = registerBlock("cyan_carpet_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.CARPET).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block PURPLE_CARPET_TRAPDOOR = registerBlock("purple_carpet_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.CARPET).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block BLUE_CARPET_TRAPDOOR = registerBlock("blue_carpet_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.CARPET).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block BROWN_CARPET_TRAPDOOR = registerBlock("brown_carpet_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.CARPET).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block GREEN_CARPET_TRAPDOOR = registerBlock("green_carpet_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.CARPET).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block BLACK_CARPET_TRAPDOOR = registerBlock("black_carpet_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.CARPET).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-
-    public static final Block RED_CARPET_IRON_TRAPDOOR = registerBlock("red_carpet_iron_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.METAL).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block WHITE_CARPET_IRON_TRAPDOOR = registerBlock("white_carpet_iron_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.METAL).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block ORANGE_CARPET_IRON_TRAPDOOR = registerBlock("orange_carpet_iron_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.METAL).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block MAGENTA_CARPET_IRON_TRAPDOOR = registerBlock("magenta_carpet_iron_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.METAL).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block LIGHT_BLUE_CARPET_IRON_TRAPDOOR = registerBlock("light_blue_carpet_iron_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.METAL).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block YELLOW_CARPET_IRON_TRAPDOOR = registerBlock("yellow_carpet_iron_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.METAL).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block LIME_CARPET_IRON_TRAPDOOR = registerBlock("lime_carpet_iron_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.METAL).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block PINK_CARPET_IRON_TRAPDOOR = registerBlock("pink_carpet_iron_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.METAL).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block GRAY_CARPET_IRON_TRAPDOOR = registerBlock("gray_carpet_iron_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.METAL).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block LIGHT_GRAY_CARPET_IRON_TRAPDOOR = registerBlock("light_gray_carpet_iron_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.METAL).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block CYAN_CARPET_IRON_TRAPDOOR = registerBlock("cyan_carpet_iron_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.METAL).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block PURPLE_CARPET_IRON_TRAPDOOR = registerBlock("purple_carpet_iron_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.METAL).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block BLUE_CARPET_IRON_TRAPDOOR = registerBlock("blue_carpet_iron_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.METAL).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block BROWN_CARPET_IRON_TRAPDOOR = registerBlock("brown_carpet_iron_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.METAL).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block GREEN_CARPET_IRON_TRAPDOOR = registerBlock("green_carpet_iron_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.METAL).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-    public static final Block BLACK_CARPET_IRON_TRAPDOOR = registerBlock("black_carpet_iron_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.METAL).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
-
-
+    public static final String[] colors = new String[] {"white", "orange", "magenta", "light_blue", "yellow", "lime", "pink", "gray", "light_gray", "cyan", "purple", "blue", "brown", "green", "red", "black"};
 
     public static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
@@ -60,7 +28,20 @@ public class ModBlocks {
     }
     public static void registerModBlocks()
     {
-        System.out.println("Registering Carpet Trapdoors");
+        registerWoodenTrapdoors();
+        registerIronTrapdoors();
     }
 
+    public static void registerWoodenTrapdoors()
+    {
+        for (String color: colors) {
+            registerBlock(color+"_carpet_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.CARPET).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
+        }
+    }
+    public static void registerIronTrapdoors()
+    {
+        for (String color: colors) {
+            registerBlock(color+"_carpet_iron_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.of(Material.METAL).strength(3.0f, 3.0f).sounds(BlockSoundGroup.WOOL)));
+        }
+    }
 }
